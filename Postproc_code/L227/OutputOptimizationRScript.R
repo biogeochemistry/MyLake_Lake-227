57bafc624d95ade4af2f0b888579d52c6ba08a99
KateriSalk <kateri.salk@gmail.com>
2017-12-21 18:33:47 -0500

    Working on a new optimization script for MyLake

---
+++ Postproc_code/L227/OutputOptimizationRScript.R
@@ -0,0 +1,22 @@
+
+setwd("/Users/krsalkgu/Documents/SourceTree/Lake227/Postproc_code/L227")
+
+integratedepi = read.csv("Observed_IntegratedEpi_icefree.csv")
+attach(integratedepi)
+head(integratedepi)
+integratedepi$date = as.Date(integratedepi$date, "%m/%d/%y")
+O2 = read.csv("Observed_Oxygen.csv")
+attach(O2)
+head(O2)
+O2$date = as.Date(O2$date, "%m/%d/%y")
+
+library(tidyverse)
+
+OutputForOptimization = merge(integratedepi, O2, by = "date", all=T)
+OutputForOptimization_Simple = select(OutputForOptimization, date, Obs_TP:Obs_TDP, obs.O2.4m:obs.O2.10m)
+OutputForOptimization_Dates = mutate(OutputForOptimization_Simple, 
+                                     Year = as.numeric(format(date, format="%Y")), 
+                                     Month = as.numeric(format(date, format="%m")), 
+                                     Day = as.numeric(format(date, format="%d")))
+
+write.csv(OutputForOptimization_Dates, file = "OutputForOptimization.csv", row.names = F, na = "")
